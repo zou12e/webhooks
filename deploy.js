79758a745068d3889c659653f4aca0d2bf9fc972
jeff <281933726@qq.com>
2018-02-27 23:15:38 +0800

    todu

--- deploy.js
+++ deploy.js
@@ -1,34 +1,34 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/', secret: 'wobuzhidao' })
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({ path: '/', secret: 'wobuzhidao' });
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
-function run_cmd(cmd, args, callback) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var resp = "";
+function runCmd (cmd, args, callback) {
+    const spawn = require('child_process').spawn;
+    const child = spawn(cmd, args);
+    let resp = '';
 
-  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-  child.stdout.on('end', function() { callback (resp) });
+    child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
+    child.stdout.on('end', function () { callback(resp); });
 }
 
 http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(9999)
+    handler(req, res, function (err) {
+        console.log(err);
+        res.statusCode = 404;
+        res.end('no such location');
+    });
+}).listen(9999);
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+    console.error('Error:', err.message);
+});
 
 // 当push代码时候执行
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref);
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref);
     // 运行.sh脚本
-    run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
-})
-
+    runCmd('sh', ['./deploy.sh', event.payload.repository.name], function (text) { console.log(text); });
+});
