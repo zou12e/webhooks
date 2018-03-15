8fb51f1e2d2756a6c92136bef8179004e75f0b15
zou12e <281933726@qq.com>
2018-03-15 15:49:54 +0800

    webhooks

--- deploy.js
+++ deploy.js
@@ -1,34 +1,26 @@
 const http = require('http');
-const createHandler = require('github-webhook-handler');
-const handler = createHandler({ path: '/', secret: 'wobuzhidao' });
-// 上面的 secret 保持和 GitHub 后台设置的一致
+const secret = "wobuzhidao"
 
 function runCmd(cmd, args, callback) {
     const spawn = require('child_process').spawn;
     const child = spawn(cmd, args);
     let resp = '';
-
     child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
     child.stdout.on('end', function () { callback(resp); });
 }
 
 http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-        console.log(err);
-        res.statusCode = 404;
-        res.end('no such location');
-    });
+   
+    if (req.headers['x-gitlab-event'] === 'Push Hook' && req.headers['x-gitlab-token'] === secret){
+        runCmd('sh', ['./deploy.sh'], function (text) { console.log(text); });
+        res.end('ok');
+    } else {
+        res.end('hello word!');
+    }
 }).listen(9527);
 
-handler.on('error', function (err) {
-    console.error('Error:', err.message);
-});
 
 // 当push代码时候执行
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref);
-    // 运行.sh脚本
-    runCmd('sh', ['./deploy.sh', event.payload.repository.name], function (text) { console.log(text); });
-});
+
+   
+
